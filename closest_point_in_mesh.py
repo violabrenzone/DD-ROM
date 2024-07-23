732a9f6c192167e4222d279e08d07a4b490e7de7
Viola Brenzone <126099936+violabrenzone@users.noreply.github.com>
2024-07-23 16:08:40 +0200

    Add files via upload

---
+++ closest_point_in_mesh.py
@@ -0,0 +1,66 @@
+import numpy as np
+import dolfin as dl
+from closest_point_on_simplex import closest_point_on_simplex
+
+def closest_point_in_mesh(p, mesh):
+    """Finds the nearest point in a mesh to a given point
+    p is the point or points (numpy array)
+    mesh is the fenics/dolfin mesh
+
+    p.shape = (N,) OR (num_pts, N) for vectorization over many points
+    N = dimension of ambient space
+
+    Example usage:
+        import numpy as np
+        import dolfin as dl
+        import matplotlib.pyplot as plt
+        mesh = dl.UnitSquareMesh(13,9)
+        num_pts = 20
+        p = np.random.randn(num_pts, 2) + np.array([0.5, 0.5])
+        closest_p = closest_point_in_mesh(p, mesh)
+        plt.figure()
+        dl.plot(mesh)
+        for ii in range(num_pts):
+            plt.plot([p[ii,0], closest_p[ii,0]], [p[ii,1], closest_p[ii,1]], 'b')
+            plt.plot(p[ii,0], p[ii,1], '*k')
+            plt.plot(closest_p[ii,0], closest_p[ii,1], '.r')
+        plt.show()
+    """
+    if len(p.shape) == 1:
+        PP = p[None,:]
+    else:
+        PP = p
+    num_pts, N = PP.shape
+    tdim = mesh.topology().dim()
+    k = tdim + 1
+
+    VV = np.zeros((num_pts, k, N))
+    bbt = mesh.bounding_box_tree()
+    for ii in range(num_pts):
+        pi = PP[ii,:]
+        closest_entity, closest_distance = bbt.compute_closest_entity(dl.Point(pi))
+        closest_cell = mesh.cells()[closest_entity]
+        vertices_of_closest_cell = mesh.coordinates()[closest_cell, :]
+        VV[ii, :, :] = vertices_of_closest_cell
+
+    closest_PP = closest_point_on_simplex(PP, VV)
+
+    if len(p.shape) == 1:
+        closest_PP = closest_PP.reshape(-1)
+    return closest_PP
+
+'''import numpy as np
+import dolfin as dl
+import matplotlib.pyplot as plt
+mesh = dl.UnitSquareMesh(13,9)
+num_pts = 20
+p = np.random.randn(num_pts, 2) + np.array([0.5, 0.5])
+closest_p = closest_point_in_mesh(p, mesh)
+plt.figure()
+dl.plot(mesh)
+for ii in range(num_pts):
+    plt.plot([p[ii,0], closest_p[ii,0]], [p[ii,1], closest_p[ii,1]], 'b')
+    plt.plot(p[ii,0], p[ii,1], '*k')
+    plt.plot(closest_p[ii,0], closest_p[ii,1], '.r')
+plt.show()'''
+ 
